c1b1cca1df6d0498ae9b21fb20c58f926ffa274b
Jake Battani <jake45rug@gmail.com>
2024-11-26 13:56:16 -0600

    Initialized project

---
+++ common/defines/00_colonies+.lua
@@ -0,0 +1,5 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NCountry.MAX_CROWN_COLONIES = 0
---
+++ descriptor.mod
@@ -0,0 +1,9 @@
+version="pre-0.0"
+tags={
+	"Gameplay"
+	"New Nations"
+	"Expansion"
+	"Historical"
+}
+name="Colonies+"
+supported_version="1.37.5.0"
